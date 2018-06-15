c7fd921a0c0437b4f85e50e34d16bcde155c11aa
tanvikchw <39824811+tanvikchw@users.noreply.github.com>
2018-06-15 13:04:55 +0530

    Create FirstComponent.tsx

---
+++ components/FirstComponent.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+
+
+ class FirstComponent extends React.Component {
+  public render() {
+    return (
+      <div className="FirstComponent">
+       <h1>BMI CALCULATOR</h1>
+      </div>
+    );
+  }
+}
+
+export default FirstComponent;
